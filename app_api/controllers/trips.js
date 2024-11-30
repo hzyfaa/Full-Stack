930bee3029bb97929381c2196bab85f8648b8f00
hzyfaa <ahuzayfa14@gmail.com>
2024-11-30 14:33:12 -0700

    Module 5 completed baseline

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr');
+const Model = mongoose.model('trips');
+
+// responses must include HTML status code and JSON message
+// GET: /trips - list all the trips
+const tripsList = async (req, res) => {
+    // find all records
+    const q = await Model.find({}).exec();
+
+    if (!q) {
+        return res.status(404).json(err);
+    } else {
+        return res.status(200).json(q);
+    }
+}
+
+// GET: /trips/:tripCode - list single trip
+const tripsFindByCode = async (req, res) => {
+    const q = await Model.find({ 'code': req.params.tripCode }).exec();
+
+    if (!q) {
+        return res.status(404).json(err);
+    } else {
+        return res.status(200).json(q);
+    }
+}
+
+module.exports = { tripsList, tripsFindByCode };
